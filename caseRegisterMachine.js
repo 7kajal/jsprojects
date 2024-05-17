724f7f28d93063f4262e7d89e824003e81ae22ec
Kajal Yadav <143157675+7kajal@users.noreply.github.com.>
2024-05-17 19:50:26 +0530

    Incomplete

---
+++ caseRegisterMachine.js
@@ -0,0 +1,10 @@
+const cashRegister = {
+            itemsForSale : [
+            {Name :"Phone" , Prize :300 },
+            {Name :"Smart Tv" , Prize :220 },
+            {Name :"Gaming Console" , Prize :150 },
+        ],
+            shoppingCart : []
+};
+
+
